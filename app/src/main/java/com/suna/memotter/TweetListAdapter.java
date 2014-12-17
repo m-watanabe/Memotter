bf2d49f028e95ac3392c91215190c7afb6aa1816
m-watanabe <masaru.nogami@gmail.com>
2014-12-17 16:55:40 +0900

    Add network permission and add view holder.

--- app/src/main/java/com/suna/memotter/TweetListAdapter.java
+++ app/src/main/java/com/suna/memotter/TweetListAdapter.java
@@ -25,6 +25,26 @@ public class TweetListAdapter extends ArrayAdapter<TweetDataRow> {
                     DateUtils.FORMAT_SHOW_WEEKDAY | DateUtils.FORMAT_SHOW_YEAR |
                     DateUtils.FORMAT_ABBREV_ALL;
 
+    private static class ViewHolder {
+        TextView user_screen_name;
+        TextView user_name;
+        TextView user_id;
+        TextView tweet_text;
+        TextView create_at;
+        TextView client_name;
+        ImageView imageView;
+
+        public ViewHolder(View view) {
+            this.user_screen_name =  (TextView) view.findViewById(R.id.user_screen_name);
+            this.user_name =  (TextView) view.findViewById(R.id.user_name);
+            this.user_id =  (TextView) view.findViewById(R.id.user_id);
+            this.tweet_text =  (TextView) view.findViewById(R.id.tweet_text);
+            this.create_at =  (TextView) view.findViewById(R.id.create_at);
+            this.client_name =  (TextView) view.findViewById(R.id.client_name);
+            this.imageView =  (ImageView) view.findViewById(R.id.user_profile_image);
+        }
+    }
+
     public TweetListAdapter(Context context, int textViewResourceId, List<TweetDataRow> objects, DBAdapter DBAdapter) {
         super(context, textViewResourceId, objects);
         mContext = context;
@@ -35,42 +55,27 @@ public class TweetListAdapter extends ArrayAdapter<TweetDataRow> {
 
     @Override
     public View getView(int position, View convertView, ViewGroup parent) {
+        ViewHolder holder;
         TweetDataRow item = (TweetDataRow) getItem(position);
 
         if (null == convertView) {
             convertView = Inf.inflate(R.layout.tweet_list_row, null);
+            holder = new ViewHolder(convertView);
+            convertView.setTag(holder);
+        } else {
+            holder = (ViewHolder) convertView.getTag();
         }
 
         // CustomDataのデータをViewの各Widgetにセットする
-        TextView user_screen_name;
-        user_screen_name = (TextView) convertView.findViewById(R.id.user_screen_name);
-        user_screen_name.setText(item.getUser_screen_name());
-
-        TextView user_name;
-        user_name = (TextView) convertView.findViewById(R.id.user_name);
-        user_name.setText(item.getUser_name());
-
-        TextView user_id;
-        user_id = (TextView) convertView.findViewById(R.id.user_id);
-        user_id.setText(item.getUser_id());
-
-        TextView tweet_text;
-        tweet_text = (TextView) convertView.findViewById(R.id.tweet_text);
-        tweet_text.setText(item.getTweetText());
-
-        TextView create_at;
-        create_at = (TextView) convertView.findViewById(R.id.create_at);
+        holder.user_screen_name.setText(item.getUser_screen_name());
+        holder.user_name.setText(item.getUser_name());
+        holder.user_id.setText(item.getUser_id());
+        holder.tweet_text.setText(item.getTweetText());
         String timeString = item.getCreate_at();
-        create_at.setText(DateUtils.formatDateTime(mContext, Long.parseLong(timeString), FLAGS));
-
-        TextView client_name;
-        client_name = (TextView) convertView.findViewById(R.id.client_name);
-        client_name.setText(item.getClient_name() + " から");
-
-        ImageView imageView;
-        imageView = (ImageView) convertView.findViewById(R.id.user_profile_image);
+        holder.create_at.setText(DateUtils.formatDateTime(mContext, Long.parseLong(timeString), FLAGS));
+        holder.client_name.setText(item.getClient_name() + " から");
         Picasso.with(mContext).setLoggingEnabled(true);
-        Picasso.with(mContext).load(item.getUser_profile_image_url()).error(R.drawable.ic_launcher).fit().into(imageView);
+        Picasso.with(mContext).load(item.getUser_profile_image_url()).error(R.drawable.ic_launcher).fit().into(holder.imageView);
 
         ImageView cardMenu = (ImageView) convertView.findViewById(R.id.card_menu);
         ImageView cardMenuDelete = (ImageView) convertView.findViewById(R.id.card_menu_delete);
